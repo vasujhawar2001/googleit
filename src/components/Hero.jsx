387e5a6f2c4767ef6b698b6f6d7eeb13f454d35c
Vasu Jhawar <vasujhawar2001@gmail.com>
2023-09-05 00:40:43 +0530

    correct mt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import GoogleSearchForm from './GoogleSearchForm';
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center mt-56 flex-grow'>
+    <div className='flex flex-col items-center sm:mt-56 mt-40 flex-grow'>
       <Image src="/google.png" alt="Google"
         height={180} 
         width={180}/>
