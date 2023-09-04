1c6b2c1cc2a16c8a708c2b3a9d3c9855ba144b9a
Vasu Jhawar <vasujhawar2001@gmail.com>
2023-09-05 00:31:58 +0530

    cahnged reponame and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import GoogleSearchForm from './GoogleSearchForm';
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center sm:mt-42 mt-28 flex-grow'>
+    <div className='flex flex-col items-center mt-56 flex-grow'>
       <Image src="/google.png" alt="Google"
         height={180} 
         width={180}/>
